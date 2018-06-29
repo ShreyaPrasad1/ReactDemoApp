a02a1c92b31ab7d47b274f0ea1d46eb494c261a3
ShreyaPrasad1 <prasad.shreya44@gmail.com>
2018-06-29 21:21:17 +0530

    Add files via upload

---
+++ src/serviceUrl/serviceUrl.js
@@ -0,0 +1,17 @@
+export const BaseUrl = {
+
+    //dev Url
+    //RestUrl: 'http://localhost:5000/',
+
+    //lms Url
+    LMSUrl:'http://145.239.254.72:8080/login/index.php',
+
+    //Dpia Url
+    DPIARestUrl: 'http://145.239.254.72:5000/',
+
+    //prod Url  
+    RestUrl: "http://145.239.254.72:5000/",
+
+    BloodhoundURL: "http://145.239.254.72:8000/"
+
+};
